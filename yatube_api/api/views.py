b9ccd5fc8c14d43fbd55838f962fbd566f28e6e8
Ирина Смирнова <ismirnova83@yandex.ru>
2022-07-23 01:11:39 +0300

    22.07

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -15,6 +15,23 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+    def partial_update(self, request, pk):
+        post = get_object_or_404(Post, id=pk)
+        if request.user == post.author:
+            serializer = self.serializer_class(post, data=request.data)
+            if serializer.is_valid():
+                serializer.save(author=self.request.user, post=post)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_403_FORBIDDEN)
+
+    def destroy(self, request, pk):
+        post = get_object_or_404(Post, id=pk)
+        if request.user == post.author:
+            post.delete()
+            return Response('Post delete', status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_403_FORBIDDEN)        
+
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
@@ -29,27 +46,28 @@ class CommentViewSet(viewsets.ModelViewSet):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         return post.comments
         
-    def create(self, request, pk):
-        post = get_object_or_404(Post, id=pk)
+    def create(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save(author=self.request.user, post=post)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    def update(self, request, pk, pk_comment):
-        post = get_object_or_404(Post, id=pk)
-        comment = get_object_or_404(self.queryset, id=pk_comment, post=pk)
-        serializer = self.serializer_class(comment, data=request.data)
+    def partial_update(self, request, post_id, pk):
+        post = get_object_or_404(Post, id=post_id)
+        comment = get_object_or_404(self.queryset, id=pk, post=post)
         if request.user == comment.author:
+            serializer = self.serializer_class(comment, data=request.data)
             if serializer.is_valid():
                 serializer.save(author=self.request.user, post=post)
                 return Response(serializer.data, status=status.HTTP_200_OK)
             return Response(status=status.HTTP_400_BAD_REQUEST)
         return Response(status=status.HTTP_403_FORBIDDEN)
 
-    def destroy(self, request, pk, pk_comment):
-        comment = get_object_or_404(self.queryset, id=pk_comment, post=pk)
+    def destroy(self, request, post_id, pk):
+        post = get_object_or_404(Post, id=post_id)
+        comment = get_object_or_404(self.queryset, id=pk, post=post)
         if request.user == comment.author:
             comment.delete()
             return Response('Comment delete', status=status.HTTP_204_NO_CONTENT)
